de5a5cf19a51d56ef6a8fa0b943d90c6a52f0fa5
Anton Konev <evilj0e@yandex-team.ru>
2019-08-21 16:09:12 +0500

    chore: add links to tasks

--- codewars solutions/function-composition.js
+++ codewars solutions/function-composition.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5655c60db4c2ce0c2e000026
+
 function compose(...args) {
   return param => args.reverse().reduce((ar, func) => func(ar), param);
 }
--- codewars solutions/reusable-memoisation.js
+++ codewars solutions/reusable-memoisation.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5b773b698adeaeb6b80000df
+
 function memo(fn) {
   const cache = new Map();
 
